3757f30374ac8cb89b5d169bb03e036ff52e2290
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-11 02:19:32 +0000

    square pages complete

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -7,4 +7,10 @@ class CalculatorController < ApplicationController
   def square_new
     render({ :template => "calculator_templates/square_new" })
   end
+
+  def square_results
+    @num = params.fetch("number").to_f
+    @num_squared = @num ** 2
+    render ({ :template => "calculator_templates/square_results" })
+  end
 end
