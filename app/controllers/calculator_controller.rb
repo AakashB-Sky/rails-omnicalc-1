14c3b26406d6676d866f8bd08cd9707d944c7d70
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-11 02:06:05 +0000

    homepage to square action

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,9 @@
 class CalculatorController < ApplicationController
+  def home
+    redirect_to "/square/new"
+  end
+
+  
   def square
     render({ :template => "calculator_templates/square" })
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "calculator", :action => "square" }) # routing to action => square because that's goal of assignment
   get("/square/new", { :controller => "calculator", :action => "square" })
 end
